15637b22610bd83331ffa827429e4ba411c2c154
Dilcia Mercedes <dilcia19@stanford.edu>
2020-01-20 01:19:18 -0800

    tried api call

--- tasks/invites.py
+++ tasks/invites.py
@@ -7,8 +7,18 @@ from invoke import task
 @task()
 def list(c):
     "Show GitHub invitations"
-    api_key = os.environ['GITHUB_API_KEY']
+    # api_key = os.environ['GITHUB_API_KEY']
+    api_key = 'bla'
     g = github.Github(api_key)
+    # a = g.get_pending_invitations()
+    # a = g.invitations()
+    # a = github.Repository.Repository.get_pending_invitations()
+    # a = github.AuthenticatedUser.AuthenticatedUser.get_invitations()
+    # a = github.AuthenticatedUser.AuthenticatedUser.get_invitations()
+
+    # b = g.get_invitations() # api - doesn't work
+    # a = g.get_organizations() # works - main class
+    # print(b)
     # TODO: list invites
     # https://developer.github.com/v3/repos/invitations/
     # https://developer.github.com/v3/repos/invitations/#list-a-users-repository-invitations
