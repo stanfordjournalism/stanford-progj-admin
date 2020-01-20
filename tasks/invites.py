8ad1923c9f38617b7c6e3476eb7f96581346f5c9
Dilcia Mercedes <dilcia19@stanford.edu>
2020-01-20 11:05:37 -0800

    accept invite function added

--- tasks/invites.py
+++ tasks/invites.py
@@ -14,7 +14,10 @@ def list(c):
     me = g.get_user()
     invites_list = me.get_invitations()
     for repo_invite in invites_list:
-        print(repo_invite)
+        # print(repo_invite)
+        me.accept_invitation(repo_invite)
+        print(repo_invite, ' has been accepted.')
+
 
     # TODO: list invites
     # https://developer.github.com/v3/repos/invitations/
@@ -24,8 +27,10 @@ def list(c):
 @task()
 def accept(c):
     "Accept one or more GitHub invites"
-    api_key = os.environ['GITHUB_API_KEY']
+    # api_key = os.environ['GITHUB_API_KEY']
+    api_key = 'bla'
     g = github.Github(api_key)
+
     # TODO: accept invites
     # https://pygithub.readthedocs.io/en/latest/github_objects/Invitation.html
     # https://developer.github.com/v3/repos/invitations/
