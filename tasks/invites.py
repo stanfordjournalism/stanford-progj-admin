9f91e53e992338b6ecdf42c44098b2fb634ed360
Serdar Tumgoren <zstumgoren@gmail.com>
2020-01-22 08:59:21 -0800

    Print human-readable names of invites #3

--- tasks/invites.py
+++ tasks/invites.py
@@ -8,35 +8,19 @@ from invoke import task
 def list(c):
     "Show GitHub invitations"
     api_key = os.environ['GITHUB_API_KEY']
-    # api_key = 'bla'
     g = github.Github(api_key)
-  
     me = g.get_user()
     invites_list = me.get_invitations()
     for repo_invite in invites_list:
-        print(repo_invite)
-
-    # TODO: list invites
-    # https://developer.github.com/v3/repos/invitations/
-    # https://developer.github.com/v3/repos/invitations/#list-a-users-repository-invitations
-    # https://pygithub.readthedocs.io/en/latest/github_objects/Invitation.html
+        print(repo_invite.html_url)
 
 @task()
 def accept(c):
     "Accept one or more GitHub invites"
     api_key = os.environ['GITHUB_API_KEY']
-    # api_key = 'bla'
     g = github.Github(api_key)
-
     me = g.get_user()
     invites_list = me.get_invitations()
     for repo_invite in invites_list:
-
         me.accept_invitation(repo_invite)
-        print(repo_invite, ' has been accepted.')
-        
-
-    # TODO: accept invites
-    # https://pygithub.readthedocs.io/en/latest/github_objects/Invitation.html
-    # https://developer.github.com/v3/repos/invitations/
-    # https://developer.github.com/v3/repos/invitations/#accept-a-repository-invitation
+        print(repo_invite.html_url, ' has been accepted.')
